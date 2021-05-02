d5f019ac9d0810d180bdd0fbcc1f0589a7375e06
Ashan-Chathuranga <83392503+Ashan-Chathuranga@users.noreply.github.com>
2021-05-02 21:28:20 +0530

    BeMe Chat App

---
+++ app/src/main/java/com/example/bemechat/Activity/ProfileInfoActivity.java
@@ -0,0 +1,144 @@
+package com.example.bemechat.Activity;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.bemechat.Models.User;
+import com.example.bemechat.databinding.ActivityProfileInfoBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+public class ProfileInfoActivity extends AppCompatActivity {
+
+    ActivityProfileInfoBinding binding;
+    FirebaseAuth auth;
+    FirebaseStorage storage;
+    FirebaseDatabase database;
+    Uri selectedImage;
+    ProgressDialog dialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityProfileInfoBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        database = FirebaseDatabase.getInstance();
+        storage = FirebaseStorage.getInstance();
+        auth = FirebaseAuth.getInstance();
+
+        dialog = new ProgressDialog(this);
+        dialog.setMessage("Updating Profile...");
+        dialog.setCancelable(false);
+
+        database = FirebaseDatabase.getInstance();
+        storage = FirebaseStorage.getInstance();
+        auth = FirebaseAuth.getInstance();
+
+        binding.UserImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent,45);
+            }
+        });
+        binding.SetupProfileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = binding.UserInfoEt.getText().toString();
+
+                if (name.isEmpty()){
+                    binding.UserInfoEt.setError("Please type your name");
+                    return;
+                }
+
+                dialog.show();
+
+                if (selectedImage != null){
+                    StorageReference reference = storage.getReference().child("Profiles").child(auth.getUid());
+                    reference.putFile(selectedImage).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                            if(task.isSuccessful()){
+                                reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                    @Override
+                                    public void onSuccess(Uri uri) {
+                                        String imageUrl = uri.toString();
+
+                                        String uid = auth.getUid();
+                                        String phone = auth.getCurrentUser().getPhoneNumber();
+                                        String name = binding.UserInfoEt.getText().toString();
+
+                                        User user = new User(uid, name, phone, imageUrl);
+
+                                        database.getReference()
+                                                .child("users")
+                                                .child(uid)
+                                                .setValue(user)
+                                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                    @Override
+                                                    public void onSuccess(Void aVoid) {
+                                                        dialog.dismiss();
+                                                        Intent intent = new Intent(ProfileInfoActivity.this,MainActivity.class);
+                                                        startActivity(intent);
+                                                        finish();
+                                                    }
+                                                });
+                                    }
+                                });
+                            }
+                        }
+                    });
+                }
+                else {
+                    String uid = auth.getUid();
+                    String phone = auth.getCurrentUser().getPhoneNumber();
+
+                    User user = new User(uid, name, phone, "No Image");
+
+                    database.getReference()
+                            .child("users")
+                            .child(uid)
+                            .setValue(user)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    dialog.dismiss();
+                                    Intent intent = new Intent(ProfileInfoActivity.this,MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            });
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (data != null){
+            if (data.getData() != null){
+                binding.UserImage.setImageURI(data.getData());
+                selectedImage = data.getData();
+            }
+        }
+
+    }
+}
